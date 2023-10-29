7992dfc1a320eb44fe36d4add76f4ff5bfee3d27
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-10-29 11:45:35 +0100

    feat: removes the avatar for now

--- components/molecules/Hero.tsx
+++ components/molecules/Hero.tsx
@@ -11,14 +11,15 @@ const Hero = () => (
       }
       description="Hello! I'm Fabio Carpinato, a skilled Software Engineer with 6 years of experience."
       avatar={
-        <Image
-          className="h-80 w-64"
-          src="/assets/images/avatar.svg"
-          alt="Avatar image"
-          loading="lazy"
-          width={100}
-          height={100}
-        />
+        // <Image
+        //   className="h-80 w-64"
+        //   src="/assets/images/avatar.svg"
+        //   alt="Avatar image"
+        //   loading="lazy"
+        //   width={100}
+        //   height={100}
+        // />
+        <></>
       }
     />
   </Section>
